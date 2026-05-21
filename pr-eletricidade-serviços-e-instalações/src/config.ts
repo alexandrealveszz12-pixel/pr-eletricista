77f511a6c51b710bbb7b625171c7293ed0a583b5
alexandrealveszz12-pixel <alexandrealveszz12@gmail.com>
2026-05-21 10:30:49 -0300

    Delete pr-eletricidade-serviços-e-instalações directory

--- pr-eletricidade-serviços-e-instalações/src/config.ts
+++
@@ -1,6 +0,0 @@
-export const COMPANY_NAME = "PR ELETRICIDADE SERVIÇOS E INSTALAÇÕES LTDA";
-export const WHATSAPP_NUMBER = "5561996802219";
-export const WHATSAPP_DISPLAY = "+55 61 99680-2219";
-export const WHATSAPP_MESSAGE = "Olá, encontrei o site e gostaria de solicitar um orçamento.";
-
-export const WHATSAPP_URL = `https://wa.me/${WHATSAPP_NUMBER}?text=${encodeURIComponent(WHATSAPP_MESSAGE)}`;
